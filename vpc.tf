3905f154d579c3e68b10d525789c427bd875aa00
Siravich-Jitpremvanich <siravich.jitpremvanich@ibm.com>
2026-03-13 14:35:37 +0700

    update vpc

--- vpc.tf
+++ vpc.tf
@@ -46,7 +46,7 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.demo-vpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "ap-southeast-2a"
+  availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1"
@@ -56,7 +56,7 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.demo-vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-southeast-2b"
+  availability_zone       = "ap-southeast-1b"
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-2"
@@ -66,7 +66,7 @@ resource "aws_subnet" "public-subnet-2" {
 resource "aws_subnet" "private-subnet-1" {
   vpc_id            = aws_vpc.demo-vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-2a"
+  availability_zone = "ap-southeast-1a"
 
   tags = {
     Name = "private-subnet-1"
@@ -76,7 +76,7 @@ resource "aws_subnet" "private-subnet-1" {
 resource "aws_subnet" "private-subnet-2" {
   vpc_id            = aws_vpc.demo-vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "ap-southeast-2b"
+  availability_zone = "ap-southeast-1b"
 
   tags = {
     Name = "private-subnet-2"
