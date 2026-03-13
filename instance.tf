92be80b8dfd85b6293e1a2b2047b62d33ee094ff
Siravich-Jitpremvanich <siravich.jitpremvanich@ibm.com>
2026-03-13 15:11:24 +0700

    update aws ami

--- instance.tf
+++ instance.tf
@@ -1,36 +1,52 @@
+# Data source to fetch the latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["684018405375"] # Canonical's AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Bastion host
 resource "aws_instance" "bastion" {
-  ami           = "ami-0279a86684f669718"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  subnet_id              = aws_subnet.public-subnet-1.id # ใช้ subnet ที่สร้างใน VPC
+  subnet_id              = aws_subnet.public-subnet-1.id
 
   depends_on = [aws_internet_gateway.gw, local_file.deployer_key]
 
   provisioner "local-exec" {
     command = <<EOT
       echo "hi"
-      # Set the permissions on the private key
       chmod 600 ${local_file.deployer_key.filename}
-      
-      # SCP the private key to the Bastion Host
       sleep 15
       scp -o StrictHostKeyChecking=no -i ${local_file.deployer_key.filename} ${local_file.deployer_key.filename} ubuntu@${aws_instance.bastion.public_ip}:/home/ubuntu/
     EOT
   }
+
   tags = {
     Name = "bastion"
   }
 }
 
+# EC2 instances in private subnets
 resource "aws_instance" "ec2_1" {
-  ami           = "ami-0279a86684f669718"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  subnet_id              = aws_subnet.private-subnet-1.id # ใช้ subnet ที่สร้างใน VPC
+  subnet_id              = aws_subnet.private-subnet-1.id
 
   depends_on = [aws_nat_gateway.nat]
 
@@ -48,12 +64,12 @@ resource "aws_instance" "ec2_1" {
 }
 
 resource "aws_instance" "ec2_2" {
-  ami           = "ami-0279a86684f669718"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  subnet_id              = aws_subnet.private-subnet-2.id # ใช้ subnet ที่สร้างใน VPC
+  subnet_id              = aws_subnet.private-subnet-2.id
 
   depends_on = [aws_nat_gateway.nat]
 
