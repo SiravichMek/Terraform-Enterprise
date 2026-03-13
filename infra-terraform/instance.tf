617b7983fde7008d654f6a67cadc2dd7f13aab5a
Siravich-Jitpremvanich <siravich.jitpremvanich@ibm.com>
2026-03-13 16:02:01 +0700

    update ami again

--- infra-terraform/instance.tf
+++ infra-terraform/instance.tf
@@ -6,17 +6,13 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
+ 
   }
 }
 
